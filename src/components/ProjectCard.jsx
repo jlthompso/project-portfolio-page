c6a07660a5ff4879002e5726c7d0b506a5091207
Joe Thompson <jthompson7@blueorigin.com>
2024-07-11 08:48:16 -0700

    Implement image gallery.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,10 +3,31 @@ import { useState } from 'react'
 
 function ProjectCard({ project }) {
     const [flipped, setFlipped] = useState(false)
+    const [imgIdx, setImgIdx] = useState(0)
+    const [timerId, setTimerId] = useState(null)
 
     const projectName = Object.keys(project)[0]
     const projectDescription = project[projectName].description
     const projectImages = project[projectName].images
+    const resetCard = () => {
+      setImgIdx(0)
+      setFlipped(false)
+    }
+    const setTimer = (seconds) => {
+      if (timerId) {
+        clearTimeout(timerId)
+        setTimerId(null)
+      }
+      setTimerId(setTimeout(resetCard, seconds * 1000))
+    }
+    const nextImg = () => {
+      if (imgIdx < projectImages.length - 1) {setImgIdx(imgIdx + 1)}
+      setTimer(5)
+    }
+    const prevImg = () => {
+      if (imgIdx > 0) {setImgIdx(imgIdx - 1)}
+      setTimer(5)
+    }
 
     // open links in new tab
     const ExternalLinkRenderer = (props) => <a href={props.href} target="_blank" rel="noreferrer">{props.children}</a>
@@ -18,8 +39,16 @@ function ProjectCard({ project }) {
               <img className="project-card-image" src={`images/${projectImages[0]}`}/>
             </div>
             <div className="project-card-back">
-              <h2>{projectName}</h2>
-              <Markdown className="project-card-description" components={{ a: ExternalLinkRenderer }}>{projectDescription}</Markdown>
+              {imgIdx > 0 ? <img className="project-card-image fade" src={`images/${projectImages[imgIdx]}`}/> :
+                <>
+                  <h2>{projectName}</h2>
+                  <Markdown className="project-card-description" components={{ a: ExternalLinkRenderer }}>{projectDescription}</Markdown>
+                </>
+              }
+              {projectImages.length > 1 && <div className='next-button-container'>
+                {imgIdx > 0 && <a className="prev" onClick={() => prevImg()}>&#10094;</a>}
+                {imgIdx < projectImages.length - 1 && <a className="next" onClick={() => nextImg()}>&#10095;</a>}
+              </div>}
             </div>
           </div>
       </div> 

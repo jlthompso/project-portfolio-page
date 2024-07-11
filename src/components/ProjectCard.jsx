dda015f169914ef247bf30f0fe3c8d5eefca55b1
Joe Thompson <jthompson7@blueorigin.com>
2024-07-11 13:42:36 -0700

    Switch from card flip to gallery.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,55 +2,40 @@ import Markdown from 'react-markdown'
 import { useState } from 'react'
 
 function ProjectCard({ project }) {
-    const [flipped, setFlipped] = useState(false)
     const [imgIdx, setImgIdx] = useState(0)
     const [timerId, setTimerId] = useState(null)
 
     const projectName = Object.keys(project)[0]
     const projectDescription = project[projectName].description
-    const projectImages = project[projectName].images
-    const resetCard = () => {
-      setImgIdx(0)
-      setFlipped(false)
-    }
+    const projectImages = project[projectName].images.toSpliced(1, 0, null)
     const setTimer = (seconds) => {
       if (timerId) {
         clearTimeout(timerId)
         setTimerId(null)
       }
-      setTimerId(setTimeout(resetCard, seconds * 1000))
+      setTimerId(setTimeout(() => setImgIdx(0), seconds * 1000))
     }
-    const nextImg = () => {
-      if (imgIdx < projectImages.length - 1) {setImgIdx(imgIdx + 1)}
-      setTimer(5)
-    }
-    const prevImg = () => {
-      if (imgIdx > 0) {setImgIdx(imgIdx - 1)}
-      setTimer(5)
-    }
+    const nextImg = () => {setImgIdx((imgIdx + 1) % projectImages.length); setTimer(5)}
+    const prevImg = () => {setImgIdx(imgIdx > 0 ? imgIdx - 1 : projectImages.length - 1); setTimer(5)}
 
     // open links in new tab
     const ExternalLinkRenderer = (props) => <a href={props.href} target="_blank" rel="noreferrer">{props.children}</a>
 
+    // key triggers fade each time the <img> is rendered
     return (
-        <div className='project-card'>
-          <div className={flipped ? 'project-card-inner project-card-flip' : 'project-card-inner'} onTouchStart={() => setFlipped(!flipped)}>
-            <div className="project-card-front">
-              <img className="project-card-image" src={`images/${projectImages[0]}`}/>
+      <div className='project-card'>
+        <div className='project-card-contents'>
+          {imgIdx !== 1 ? <img key={imgIdx} className="project-card-image fade" src={`images/${projectImages[imgIdx]}`}/> :
+            <div className='project-card-description-container'>
+              <h2>{projectName}</h2>
+              <Markdown className="project-card-description" components={{ a: ExternalLinkRenderer }}>{projectDescription}</Markdown>
             </div>
-            <div className="project-card-back">
-              {imgIdx > 0 ? <img className="project-card-image fade" src={`images/${projectImages[imgIdx]}`}/> :
-                <>
-                  <h2>{projectName}</h2>
-                  <Markdown className="project-card-description" components={{ a: ExternalLinkRenderer }}>{projectDescription}</Markdown>
-                </>
-              }
-              {projectImages.length > 1 && <div className='next-button-container'>
-                {imgIdx > 0 && <a className="prev" onClick={() => prevImg()}>&#10094;</a>}
-                {imgIdx < projectImages.length - 1 && <a className="next" onClick={() => nextImg()}>&#10095;</a>}
-              </div>}
-            </div>
+          }
+          <div className='next-button-container'>
+            <a className="prev" onClick={() => prevImg()}>&#10094;</a>
+            <a className="next" onClick={() => nextImg()}>&#10095;</a>
           </div>
+        </div>
       </div> 
     )
 }
